726bc4f4752e998a54f79f72b3c403b89047e90a
RONAK BEDIYA <124416368+ronakbediya310@users.noreply.github.com>
2024-05-05 16:42:59 +0530

    Math Object in js

---
+++ Math.js
@@ -0,0 +1,9 @@
+console.log (Math.PI);
+console.log(Math.max(34,45,89,90,8));
+console.log(Math.min(34,45,89,90,8));
+console.log(Math.floor(1.6));
+console.log(Math.ceil(1.1));
+console.log (Math.abs(-1));
+console.log(Math.random());
+console.log(Math.sqrt(5));
+console.log(Math.pow(2,10));
