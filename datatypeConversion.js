6d03955a001f72fa5bf5eb6e14ae83472c05ce6d
RONAK BEDIYA <124416368+ronakbediya310@users.noreply.github.com>
2024-05-05 16:41:00 +0530

    type conversion in js

---
+++ datatypeConversion.js
@@ -0,0 +1,21 @@
+let score ="42";
+
+let numberValue = Number(score);
+console.log(numberValue);
+
+let value = null;
+
+let convertedValue = Number(value); // 0 
+
+// true -> 1
+// false -> 0
+
+let isLoggedIn = 1
+isLoggedIn = Boolean(isLoggedIn);
+
+let name ="";
+booleanName = Boolean(name)// false
+
+let marks = 33;
+let stringNumber = String(marks);
+console.log(typeof stringNumber);
