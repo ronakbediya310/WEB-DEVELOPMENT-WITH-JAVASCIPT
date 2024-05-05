cf108514fb2c0e3d05a3b4e3e8a75b807af028c2
RONAK BEDIYA <124416368+ronakbediya310@users.noreply.github.com>
2024-05-05 16:40:14 +0530

    dateTime object

---
+++ dateTime.js
@@ -0,0 +1,10 @@
+let myDate = new Date();
+console.log(myDate.toString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleString());
+
+let timeStamp = Date.now();
+console.log(timeStamp);
+myDate.getTime();
+myDate.getFullYear();
+myDate.getDay();
